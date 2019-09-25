f524f454a8debced195831ad05ae392a2659b9b7
catprogrammer-cat <catprogrammer.vlad@gmail.com>
2019-09-25 21:26:34 +0300

    add lab2_1

--- lab3_1/lab3_1.cpp
+++ lab3_1/lab3_1.cpp
@@ -1,10 +1,18 @@
 
-
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	double x, result_s;// x -> varible in the formula
+					   // result_s -> result of the formula
+	int quantity;
+	cout << "Enter the varible x: ";
+	cin >> x;
+	cout << "\nEnter how many times to output: ";
+	cin >> quantity;
+	cout << "\n";
+	for (int i = 1; i <= quantity; i++) {
+		result_s = (pow(-1, i) * (2 * i - 1)) / (pow(x, i));
+		cout << "i = " << i << ", result:  " << result_s << ";" << endl;
+	}
 }
-
-
